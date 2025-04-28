d3fb5aa893ddd6636b9bce29764739ceb22aa571
Eng_AbdElRahmanAzzam <122513601+EngAbdElRahmanAzzam@users.noreply.github.com>
2025-04-28 13:50:55 +0300

    fix userinterface

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,6 +1,6 @@
 import { Document } from "mongoose";
 
-export interface IBrand extends Document {
+export interface IUser extends Document {
     username:string,
     slug:string,
     profileImg?:string,
